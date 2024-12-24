95557b4d047109bd3e8889e77a0572df06576c4a
shimizu2424 <ciit22116@g.nihon-u.ac.jp>
2024-12-24 12:16:25 +0900

    add codes

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
         //エラーがあったときは中断
     }
 
@@ -50,15 +53,24 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
     
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
-        //System.out.println(source);
+        Parser parser = new Parser(tokens);
+        //Expr expression = parser.parse();
+        List<Stmt> statements = parser.parse();
+    
+        // Stop if there was a syntax error.
+        if (hadError) return;
+    
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+            "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
@@ -66,10 +78,18 @@ public class Lox {
         hadError = true;
     }
 
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
     //loxは初期化時の文字列によって型を決める
     //tokenに分けることを字句解析という
     //tokenを見つけて区別する必要がある
     //カッコやセミコロンなどの文字を見つけたことを記録
 
-}
+}//742から
 
--- codes/com/craftinginterpreters/tool/GenerateAst.java
+++ codes/com/craftinginterpreters/tool/GenerateAst.java
@@ -18,6 +18,11 @@ public class GenerateAst {
             "Literal  : Object value",
             "Unary    : Token operator, Expr right"
         ));
+        
+        defineAst(outputDir, "Stmt", Arrays.asList(
+            "Expression : Expr expression",
+            "Print      : Expr expression"
+        ));
     }
 
     private static void defineAst(
@@ -30,15 +35,31 @@ public class GenerateAst {
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+        defineVisitor(writer, baseName, types);
 
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim(); 
             defineType(writer, baseName, className, fields);
         }
-    
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(
+        PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+        String typeName = type.split(":")[0].trim();
+        writer.println("    R visit" + typeName + baseName + "(" +
+            typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
     }
 
     private static void defineType(PrintWriter writer, String baseName,String className, String fieldList) {
@@ -54,6 +75,13 @@ public class GenerateAst {
         writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    // Visitor pattern.
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
     writer.println("    }");
 
     // Fields.
